9ae350654b9fd0a71458c321d09ef5c4d497215c
Kokaiinum <42316813+Kokaiinum@users.noreply.github.com>
2020-09-30 03:32:51 +0100

    Bump version and cleanup
    (remove unused-usings, update Harmony usage etc)

--- KK_ExpandMaleMaker/KK_ExpandMaleMaker.cs
+++ KK_ExpandMaleMaker/KK_ExpandMaleMaker.cs
@@ -1,9 +1,7 @@
 using BepInEx;
-using HarmonyLib;
 using UnityEngine;
 using KKAPI.Chara;
-using System.Linq;
-using BepInEx.Harmony;
+using System.Reflection;
 using BepInEx.Configuration;
 
 namespace KK_ExpandMaleMaker {
@@ -14,7 +12,7 @@ namespace KK_ExpandMaleMaker {
 
         public const string Name = "Expand Male Maker";
         public const string GUID = "kokaiinum.KKExpandMaleMaker";
-        public const string Version = "1.0";
+        public const string Version = "1.1";
 
         internal static MonoBehaviour instance;
 
@@ -23,19 +21,19 @@ namespace KK_ExpandMaleMaker {
         internal static ConfigEntry<bool> compatabilityMode;
 
         private void Awake() {
-            heightEnabled = Config.AddSetting("Config", "Edit Male Height", true, new ConfigDescription("If the male height can be edited. \nRequires passing a loading screen to take effect. \nDisabling this will also prevent any male cards from\nbeing loaded with edited heights.", null, new ConfigurationManagerAttributes { Order = 3 }));
+            heightEnabled = Config.Bind("Config", "Edit Male Height", true, new ConfigDescription("If the male height can be edited. \nRequires passing a loading screen to take effect. \nDisabling this will also prevent any male cards from\nbeing loaded with edited heights.", null, new ConfigurationManagerAttributes { Order = 3 }));
 
-            hairEnabled = Config.AddSetting("Config", "Edit Male Underhair", true, new ConfigDescription("If the male underhair can be edited. \n Requires passing a loading screen to take effect.", null, new ConfigurationManagerAttributes { Order = 2 }));
+            hairEnabled = Config.Bind("Config", "Edit Male Underhair", true, new ConfigDescription("If the male underhair can be edited. \n Requires passing a loading screen to take effect.", null, new ConfigurationManagerAttributes { Order = 2 }));
 
-            compatabilityMode = Config.AddSetting("Config", "Compatability Mode", true, new ConfigDescription("When enabled, male characters not specifically saved with edited\nheights will be defaulted to 60 (the default behaviour for the game).", null, new ConfigurationManagerAttributes { IsAdvanced = true, Order = 1 }));
+            compatabilityMode = Config.Bind("Config", "Compatability Mode", true, new ConfigDescription("When enabled, male characters not specifically saved with edited\nheights will be defaulted to 60 (the default behaviour for the game).", null, new ConfigurationManagerAttributes { IsAdvanced = true, Order = 1 }));
 
             instance = this;
 
-            Hooks.isDark = typeof(ChaControl).GetProperties(AccessTools.all).Any(p => p.Name == "exType");
+            Hooks.isDark = typeof(ChaInfo).GetProperty("exType", BindingFlags.Public | BindingFlags.Instance) != null;
 
             CharacterApi.RegisterExtraBehaviour<MaleHeightCompatabilityController>(GUID);
 
-            HarmonyWrapper.PatchAll(typeof(Hooks));
+            HarmonyLib.Harmony.CreateAndPatchAll(typeof(Hooks));
         }
     }
 
